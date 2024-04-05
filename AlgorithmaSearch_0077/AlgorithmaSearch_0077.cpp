9e7c826f4503aa6eaedc9146e00398d7e08222d7
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-04-05 14:46:12 +0700

    input untuk meminta ingin melanjutkan pencarian atau tidak

--- AlgorithmaSearch_0077/AlgorithmaSearch_0077.cpp
+++ AlgorithmaSearch_0077/AlgorithmaSearch_0077.cpp
@@ -57,5 +57,8 @@ void LinearSearch()
 			cout << "\n" << item << " not found in the array\n";
 		cout << "\nNumber of comparisons: " << ctr << endl;
 
+		cout << "\nContinue search (y/n): ";
+		cin >> ch;
+
 	}
 }
